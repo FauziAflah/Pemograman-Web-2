bee61c1e48f392095897ae6756a1297047935508
M Fauzi Aflah <mfauziaflah52@gmail.com>
2024-03-20 12:55:24 +0700

    pertemuan 1

---
+++ pertemuan1/Variable.php
@@ -0,0 +1,33 @@
+<?php 
+
+echo $_SERVER['SERVER_NAME'];
+
+echo "<br>";
+
+$nama = "Budi";
+echo $nama;
+echo "<br>";
+
+$nama = "Andi";
+echo $nama;
+echo "<br>";
+define("NAMA", "Budi");
+echo NAMA;
+
+echo "<br>";
+const JENIS_KELAMIN = "Laki-laki";
+
+echo JENIS_KELAMIN;
+
+
+//latihan variable
+
+$sisi = 10;
+$luas_persegi = $sisi * $sisi;
+echo "luas persegi" . $luas_persegi;
+echo  "<br>";
+echo "luas persegi : $luas_persegi";
+echo   "<br>";
+echo 'luas persegi : $luas_persegi';
+
+ 
